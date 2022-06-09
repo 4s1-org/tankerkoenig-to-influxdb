5fc37d47fc3d1cc445372006e9c56a3b7180eeb6
Steffen <steffen@4s1.de>
2022-06-09 09:51:49 +0200

    add tests

--- tankerkoenig-to-influxdb.go
+++ tankerkoenig-to-influxdb.go
@@ -1,10 +1,10 @@
 package main
 
 import (
-	"flag"
-	"fmt"
 	"encoding/json"
 	"errors"
+	"flag"
+	"fmt"
 	"io/ioutil"
 	"os"
 )
@@ -17,7 +17,7 @@ type ConfigurationStation struct {
 }
 
 type Configuration struct {
-	Stations              []ConfigurationStation `json:"stations"`
+	Stations []ConfigurationStation `json:"stations"`
 }
 
 func main() {
@@ -25,7 +25,7 @@ func main() {
 
 	// Program parameters
 	var configFileName string
-	flag.StringVar(&configFileName, "c","", "configuration file")
+	flag.StringVar(&configFileName, "c", "", "configuration file")
 	flag.Parse()
 	sourceFiles := flag.Args()
 	fmt.Printf("Tail: %+q\n", sourceFiles)
@@ -66,3 +66,128 @@ func loadConfigurationFile(configFileName string) (*Configuration, error) {
 
 	return &configuration, nil
 }
+
+// func parseFile(date time.Time, c *Configuration) {
+// 	year := fmt.Sprintf("%04d", date.Year())
+// 	month := fmt.Sprintf("%02d", date.Month())
+// 	day := fmt.Sprintf("%02d", date.Day())
+
+// 	fmt.Printf("%s-%s-%s", year, month, day)
+// 	fmt.Println()
+
+// 	srcPath := filepath.Join(c.TankerkoenigDataFolder, "prices", year, month)
+// 	srcFilename := fmt.Sprintf("%s-%s-%s-prices.csv", year, month, day)
+// 	srcFile2 := filepath.Join(srcPath, srcFilename)
+
+// 	if _, err := os.Stat(srcFile2); errors.Is(err, os.ErrNotExist) {
+// 		fmt.Println("Sourcefile \"" + srcFile2 + "\" does not exists.")
+// 		return
+// 	}
+
+// 	destPath := filepath.Join(c.CsvDataFolder, year, month)
+// 	destFilename := fmt.Sprintf("%s-%s-%s.csv", year, month, day)
+// 	destFilenameInfluxDb := fmt.Sprintf("%s-%s-%s-influxdb.txt", year, month, day)
+// 	destFile2 := filepath.Join(destPath, destFilename)
+// 	destFile2InfluxDb := filepath.Join(destPath, destFilenameInfluxDb)
+
+// 	srcFile, err := os.Open(srcFile2)
+// 	if err != nil {
+// 		log.Fatal(err)
+// 	}
+// 	defer srcFile.Close()
+
+// 	err2 := os.MkdirAll(destPath, os.ModePerm)
+// 	if err2 != nil {
+// 		log.Fatal(err2)
+// 	}
+
+// 	destFile, err := os.Create(destFile2)
+// 	if err != nil {
+// 		log.Fatal(err)
+// 	}
+// 	defer destFile.Close()
+
+// 	destFileInfluxDb, err := os.Create(destFile2InfluxDb)
+// 	if err != nil {
+// 		log.Fatal(err)
+// 	}
+// 	defer destFileInfluxDb.Close()
+
+// 	csvReader := csv.NewReader(srcFile)
+// 	csvWriter := csv.NewWriter(destFile)
+// 	defer csvWriter.Flush()
+// 	txtWriter := bufio.NewWriter(destFileInfluxDb)
+// 	defer txtWriter.Flush()
+// 	firstRow := true
+
+// 	for {
+// 		row, err := csvReader.Read()
+// 		if err == io.EOF {
+// 			break
+// 		}
+// 		if err != nil {
+// 			log.Fatal(err)
+// 		}
+
+// 		if firstRow {
+// 			header := []string{"date", "brand", "city", "street", "fueltype", "price"}
+// 			csvWriter.Write(header)
+// 			firstRow = false
+// 			continue
+// 		}
+
+// 		timestampParsed, err := time.Parse(time.RFC3339, row[0][0:10]+"T"+row[0][11:22]+":00")
+// 		if err != nil {
+// 			log.Fatal(err)
+// 		}
+// 		timestampString := timestampParsed.Format(time.RFC3339)
+// 		timestampUnix := timestampParsed.UTC().Unix()
+// 		uuid := row[1]
+
+// 		// ToDo: Das geht besser in Go
+// 		for i := 0; i < len(c.Stations); i++ {
+// 			station := c.Stations[i]
+// 			if uuid == station.Id {
+// 				parseLine(csvWriter, txtWriter, row, timestampString, timestampUnix, station)
+// 			}
+// 		}
+// 	}
+
+// 	// ToDo: Add Brennpaste
+// }
+
+// func parseLine(csvWriter *csv.Writer, txtWriter *bufio.Writer, row []string, timestampString string, timestampUnix int64, station Station) {
+// 	diesel := row[2]
+// 	e5 := row[3]
+// 	e10 := row[4]
+// 	dieselchange := row[5]
+// 	e5change := row[6]
+// 	e10change := row[7]
+
+// 	details := []string{timestampString, station.Brand, station.City, station.Street}
+
+// 	// 0=keine Änderung, 1=Änderung, 2=Entfernt, 3=Neu
+// 	// ToDo -1 und 2 beachten
+
+// 	// Hint: Manchmal ist bei einer Änderung (1) der Preis -0.001.
+// 	// Keine Ahnung warum, aber die Preise werden ignoriert.
+
+// 	if dieselchange == "1" && diesel[0] != '-' {
+// 		csvWriter.Write(append(details, "Diesel", diesel))
+// 		// ToDo Das geht besser
+// 		txtWriter.WriteString("kraftstoffpreise,marke=" + strings.Replace(station.Brand, " ", "\\ ", -1) + ",ort=" + strings.Replace(station.City, " ", "\\ ", -1) + ",strasse=" + strings.Replace(station.Street, " ", "\\ ", -1) + ",")
+// 		txtWriter.WriteString("sorte=Diesel preis=" + diesel + " " + strconv.FormatInt(timestampUnix, 10) + "\n")
+// 	}
+// 	if e5change == "1" && e5[0] != '-' {
+// 		csvWriter.Write(append(details, "E5", e5))
+// 		// ToDo Das geht besser
+// 		txtWriter.WriteString("kraftstoffpreise,marke=" + strings.Replace(station.Brand, " ", "\\ ", -1) + ",ort=" + strings.Replace(station.City, " ", "\\ ", -1) + ",strasse=" + strings.Replace(station.Street, " ", "\\ ", -1) + ",")
+// 		txtWriter.WriteString("sorte=E5 preis=" + e5 + " " + strconv.FormatInt(timestampUnix, 10) + "\n")
+// 	}
+// 	if e10change == "1" && e10[0] != '-' {
+// 		csvWriter.Write(append(details, "E10", e10))
+// 		// ToDo Das geht besser
+// 		txtWriter.WriteString("kraftstoffpreise,marke=" + strings.Replace(station.Brand, " ", "\\ ", -1) + ",ort=" + strings.Replace(station.City, " ", "\\ ", -1) + ",strasse=" + strings.Replace(station.Street, " ", "\\ ", -1) + ",")
+// 		txtWriter.WriteString("sorte=E10 preis=" + e10 + " " + strconv.FormatInt(timestampUnix, 10) + "\n")
+// 	}
+// }
